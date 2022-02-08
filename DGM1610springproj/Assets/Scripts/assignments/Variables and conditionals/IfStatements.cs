ac6ab8b8421d51389c62f09af29381c789c563a6
Esther <10889353@uvu.edu>
2022-02-08 10:30:22 -0700

    If statement assignment

---
+++ DGM1610springproj/Assets/Scripts/assignments/Variables and conditionals/IfStatements.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+    // public string LightColor;
+    // public string weather;
+    //public string haircolor;
+   // public int age;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    /*
+     if (LightColor == "red")
+     {
+         Debug.Log("The Light is " + LightColor + ", STOP!!");
+     }   
+     else if (LightColor == "yellow")
+     {
+         Debug.Log("The Light is " + LightColor + ", start to stop or clear the intersection");
+     }
+    else if (LightColor == "green")
+     {
+         Debug.Log("The Light is " + LightColor + ", GO!");
+     }
+     else
+     {
+         Debug.Log( "wrong color try again");
+     }
+     */
+    /*
+    if (weather == "rain" || weather == "snow")
+    {
+        Debug.Log("The weather is bad, you should wear a coat!");
+    }
+    else if(weather == "sunny" || weather == "partly cloudy")
+    {
+        Debug.Log("The weather is good, no need for a coat!");
+    }
+    */
+
+    /* if (haircolor = "red" && age == 6)
+    {
+        Debug.Log("you can ride the merry go round- you cant ride the fire dragon ride");
+    }
+    else if(haircolor == "blonde" && age == 100)
+    {
+        Debug.Log("you are old enough to ride the fire dragon ride");
+    }
+    */
+
+
+
+    }
+}
