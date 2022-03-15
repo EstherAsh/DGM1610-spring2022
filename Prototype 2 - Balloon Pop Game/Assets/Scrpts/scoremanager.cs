59e68e6b5be324f25134f50a1628f4f17f9a00e5
AshbyEst <10889353@uvu.edu>
2022-03-15 17:20:58 -0600

    reset computor issues, reupdated repo/balloon game

---
+++ Prototype 2 - Balloon Pop Game/Assets/Scrpts/scoremanager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class scoremanager : MonoBehaviour
+{
+    public int Score ;
+    //keep track if the score
+    public TextM
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
