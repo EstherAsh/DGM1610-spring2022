3715ada2115d52ef713ef9c6320ef7079725adb5
AshbyEst <10889353@uvu.edu>
2022-04-27 00:28:24 -0600

    RetroFPS

--- RetroFPS/Assets/Scrpts/PlayerController.cs
+++ RetroFPS/Assets/Scrpts/PlayerController.cs
@@ -4,29 +4,60 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
-    private RigidBody rb;
-    private Vector3 moveInput;
-    private Vector2 moveInput;
-    public float mouseSensitivity = 1.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-      rb = GetComponent<RigidBody>();
-      //Hie and lock curer
-      Curser.visible = fasle;
-      curser.lockState = CureserLockMode.Locked;  
-    }
+  public static PlayerController instance;
+  public float moveSpeed;
+  private Rigidbody rb;
+  private Vector3 moveInput;
+  private Vector2 mouseInput;
+  public float mouseSensitivity = 1.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-      moveInput = new Vector3(Input.GetAxis("Horizontal"),0,Input.DetAxis("Vertical"));
-      Vector3 moveH = transform.right *moveInput.x;
-      Vector3 moveV = transform.forward*moveInput.z;
-      rb.velocity = (moveH +moveV) *moveSpeed;
-      mouseInput = new vectpr3(moveInput.GetAxis("mouse X"),moveInput.GetAxis("mouse Y"))*mouseSensitivity;
-      transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, -mouseInput.x,transform.rotation.eulerAngles.z);
+  public Camera viewCam;
+  public GameObject bulletImpact;
+  public int currentAmmo;
 
+
+  private void Awake()
+  {
+    instance = this;
+  }
+  // Start is called before the first frame update
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+    //Hie and lock curer
+    Cursor.visible = false;
+    Cursor.lockState = CursorLockMode.Locked;  
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    moveInput = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));//getting Input
+    Vector3 moveH = transform.right *moveInput.x;
+    Vector3 moveV = transform.forward*moveInput.z;//nlend/diagonal movement
+    rb.velocity = (moveH +moveV) *moveSpeed;
+
+    mouseInput = new Vector2(Input.GetAxis("Mouse X"),Input.GetAxis("Mouse Y")) * mouseSensitivity;
+    transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y -mouseInput.x,transform.rotation.eulerAngles.z);//calculating rotation on x,y and z
+    if(Input.GetMouseButtonDown(0))
+    {
+      if(currentAmmo>0)
+      {
+        Ray ray = viewCam.ViewportPointToRay(new Vector3(0.5f,0.5f,0.0f));
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+          Debug.Log("Im looking at "+ hit.transform.name+".");
+          Instantiate(bulletImpact, hit.point,transform.rotation);
+        }
+        else
+        {
+          Debug.Log("Im not looking at anything");
+        }
+        currentAmmo--;
+  
+      }
     }
+    
+  }
 }
